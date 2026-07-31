5a77c4582d8ffbdb8b2c92b7705998adba838033
mckenna96 <40708834@live.napier.ac.uk>
2026-07-31 18:26:04 +0100

    App changes

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,35 +4,37 @@ import java.sql.*;
 
 public class App
 {
-    public static void main(String[] args)
-    {
-        try
-        {
-            // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
+    private Connection con = null;
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
-        for (int i = 0; i < retries; ++i)
+    public static void main(String[] args) {
+        App a = new App();
+
+        a.connect();
+        a.disconnect();
+    }
+
+        public void connect()
         {
-            System.out.println("Connecting to database...");
-            try
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            }
+            catch (ClassNotFoundException e)
             {
+                System.out.println("Could not load SQL driver");
+                System.exit(-1);
+            }
+
+            int retries = 10;
+            for (int i = 0; i < retries; ++i)
+            {
+                System.out.println("Connecting to database...");
+                try
+                {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
@@ -40,23 +42,25 @@ public class App
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
             }
-            catch (InterruptedException ie)
-            {
+            catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
+                }
             }
         }
 
-        if (con != null)
+        public void disconnect()
         {
-            try
-            {
-                // Close connection
-                con.close();
-            }
-            catch (Exception e)
-            {
-                System.out.println("Error closing connection to database");
-            }
+            if (con != null)
+                {
+                    try
+                     {
+                         // Close connection
+                         con.close();
+                      }
+                    catch (Exception e)
+                    {
+                         System.out.println("Error closing connection to database");
+                      }
+                 }
         }
-    }
 }
